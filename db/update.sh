fc47cd9f6994f0c10c392fbe6e03ad1bec3719ec
Alex Nagy <nagyalex003@gmail.com>
2026-06-17 12:49:32 +0200

    fix: update.sh wrong unzip command

--- db/update.sh
+++ db/update.sh
@@ -1,4 +1,3 @@
 mv GeoLite2-City.mmdb GeoLite2-City.mmdb.old
 wget https://cdn.jsdelivr.net/npm/geolite2-city/GeoLite2-City.mmdb.gz
-tar -xvzf GeoLite2-City.mmdb.gz
-rm GeoLite2-City.mmdb.gz
+gzip -d GeoLite2-City.mmdb.gz
